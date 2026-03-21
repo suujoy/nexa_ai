53776bdebe34b341b4925a2911b6fec39cef89e1
Sujoy Saha <ace.sujoy@gmail.com>
2026-03-21 19:10:47 +0530

    resend for mail

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,43 +1,57 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-/**
- * creating connection between SMTP server and your web server
- */
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: process.env.GOOGLE_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    },
-});
+// /**
+//  * creating connection between SMTP server and your web server
+//  */
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         type: "OAuth2",
+//         user: process.env.GOOGLE_USER,
+//         clientId: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+//     },
+// });
 
-/**
- * Veryfing transporter
- */
-transporter
-    .verify()
-    .then(() => {
-        console.log(`Email transporter is ready to send email`);
-    })
-    .catch((err) => {
-        console.error(`Email transporter verification failed`, err);
-    });
+// /**
+//  * Veryfing transporter
+//  */
+// transporter
+//     .verify()
+//     .then(() => {
+//         console.log(`Email transporter is ready to send email`);
+//     })
+//     .catch((err) => {
+//         console.error(`Email transporter verification failed`, err);
+//     });
 
-/**
- * send Email Function
- */
+// /**
+//  * send Email Function
+//  */
+// export const sendEmail = async ({ to, subject, text, html }) => {
+//     const mailOptions = {
+//         from: process.env.GOOGLE_USER,
+//         to,
+//         subject,
+//         html,
+//         text,
+//     };
+
+//     const details = await transporter.sendMail(mailOptions);
+//     console.log(details);
+// };
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async ({ to, subject, text, html }) => {
-    const mailOptions = {
-        from: process.env.GOOGLE_USER,
+    await resend.emails.send({
+        from: "Nexa AI <onboarding@resend.dev>",
         to,
         subject,
         html,
         text,
-    };
-
-    const details = await transporter.sendMail(mailOptions);
-    console.log(details);
+    });
 };
